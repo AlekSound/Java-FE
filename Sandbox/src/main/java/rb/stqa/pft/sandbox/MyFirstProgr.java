1f51ef44f5482753040f45210bca6f75c5b60cac
AlekSound <trigubovich.a@gmail.com>
2018-03-18 23:48:17 +0300

    Небольшие изменения, хз какие...

--- Sandbox/src/main/java/rb/stqa/pft/sandbox/MyFirstProgr.java
+++ Sandbox/src/main/java/rb/stqa/pft/sandbox/MyFirstProgr.java
@@ -4,6 +4,10 @@ public class MyFirstProgr {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+
+        int k = 11;
+        int m = k * k;
+      System.out.println("Площадь квадрата со стороной " + k + " = " + m);
     }
 
 }
