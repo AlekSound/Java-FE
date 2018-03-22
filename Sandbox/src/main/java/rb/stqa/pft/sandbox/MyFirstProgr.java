b453d5c2877a6c807a09ec94a0924f26ddc6d921
AlekSound <trigubovich.a@gmail.com>
2018-03-22 08:16:28 +0300

    Добавлена функция

--- Sandbox/src/main/java/rb/stqa/pft/sandbox/MyFirstProgr.java
+++ Sandbox/src/main/java/rb/stqa/pft/sandbox/MyFirstProgr.java
@@ -3,11 +3,14 @@ package rb.stqa.pft.sandbox;
 public class MyFirstProgr {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        hello("World");
+      hello("User");
+      hello("Owner");
+    }
 
-        int k = 11;
-        int m = k * k;
-      System.out.println("Площадь квадрата со стороной " + k + " = " + m);
+    public static void hello(String some) {
+      System.out.println("Hello, " + some + "!");
     }
 
 }
+
